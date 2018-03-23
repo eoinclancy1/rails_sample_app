4b32cf81f48b1801e19a5eac7063422384f0ad07
eoin clancy <eoinclancy1@yahoo.ie>
2018-03-23 02:12:39 +0000

    hello fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-    html render: "hello world!"
+    render html: "hello world!"
   end
   
 end
